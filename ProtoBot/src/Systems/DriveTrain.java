9e796ecdd97846060209ae38f9603244bd83b68a
iRobotNinja <mmassrobots3@gmail.com>
2016-11-10 18:38:47 -0800

    Proto Bot Code

---
+++ ProtoBot/src/Systems/DriveTrain.java
@@ -0,0 +1,19 @@
+package Systems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+
+public class DriveTrain {
+	static CANTalon motorL,motorR;
+	
+	// takes motor IDs and sets them to the correct motor variable
+	public DriveTrain(int leftMotorID,int rightMotorID){
+		motorL = new CANTalon(leftMotorID);
+		motorR = new CANTalon(rightMotorID);
+	}
+	
+	// call to change the power given to the motor
+	public static void ChangeSpeed(double powerL,double powerR){
+		motorL.set(powerL);
+		motorR.set(powerR);
+	}
+}
