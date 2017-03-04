931c69b78060cc83e17aaa65ec4ccdac6020de33
rtrahms <rtrahms@gmail.com>
2017-03-04 07:52:59 -0800

    Found and fixed climber assembly bug

--- Steamworks1778/src/Systems/ClimberAssembly.java
+++ Steamworks1778/src/Systems/ClimberAssembly.java
@@ -18,6 +18,7 @@ public class ClimberAssembly {
 	
 	private static CANTalon climberMotor;
 	private static Joystick gamepad;
+	private static double currentClimbValue = 0.0;
 	
 	public static void initialize() {
 		if (initialized)
@@ -26,26 +27,32 @@ public class ClimberAssembly {
 		climberMotor = new CANTalon(HardwareIDs.CLIMBER_TALON_ID);
 				
 		gamepad = new Joystick(HardwareIDs.GAMEPAD_ID);
+		
 	}
 			
 	public static void teleopInit() {
-		
+		currentClimbValue = 0.0;
 	}
 	
 	public static void teleopPeriodic() {
 
-		double climbValue = gamepad.getRawAxis(HardwareIDs.CLIMBER_MOTOR_AXIS);
+		double newClimbValue = gamepad.getRawAxis(HardwareIDs.CLIMBER_MOTOR_AXIS);
 		
-		if ((Math.abs(climbValue) < CLIMBER_MOTOR_DEAD_ZONE) || (climbValue > 0.0))
-			climbValue= 0.0;
+		// convert joystick value into motor speed value
+		if ((Math.abs(newClimbValue) < CLIMBER_MOTOR_DEAD_ZONE) || (newClimbValue > 0.0))
+			newClimbValue= 0.0;
+		else if ((Math.abs(newClimbValue) >= CLIMBER_MOTOR_DEAD_ZONE) && (newClimbValue < 0.0))
+			newClimbValue *= CLIMB_MOTOR_FACTOR;      
 		
-		if ((Math.abs(climbValue) >= CLIMBER_MOTOR_DEAD_ZONE) && (climbValue < 0.0)) {
-			climbValue *= CLIMB_MOTOR_FACTOR;      
-			climberMotor.set(climbValue);
-		}
+		// if current climber motor is set to this value already, just return
+		if (newClimbValue == currentClimbValue)
+			return;
 		
-		String climbValueStr = String.format("%.2f", climbValue);
+		// set motor and persisted climb value
+		currentClimbValue = newClimbValue;
+		climberMotor.set(newClimbValue);
+		
+		String climbValueStr = String.format("%.2f", newClimbValue);
 		InputOutputComm.putString(InputOutputComm.LogTable.kMainLog,"Climber/speed", climbValueStr);
-
 	}
 }
